0f9b2bb34c334eb2062187b99334ba432bddbad2
sweekyforzymail <sweekyforzymail@sina.com>
2019-09-30 14:38:45 +0800

    初次提交eurekaServer

---
+++ hqcloud-eurekaserver/src/main/java/com/hq/eurekaserver/EurekaServer_1.java
@@ -0,0 +1,14 @@
+package com.hq.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer_1 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServer_1.class, args);
+	}
+}
