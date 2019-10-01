a418ccfaf2c1a9149eb8127bdf036855a7660bbc
sweekyforzymail <sweekyforzymail@sina.com>
2019-10-01 12:41:25 +0800

    模块-俱乐部

--- hqcloud-club/src/main/java/com/hq/club/ApplicationClub.java
+++ hqcloud-club/src/main/java/com/hq/club/ApplicationClub.java
@@ -2,11 +2,14 @@ package com.hq.club;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ApplicationClub {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationClub.class, args);
+
 	}
 }
