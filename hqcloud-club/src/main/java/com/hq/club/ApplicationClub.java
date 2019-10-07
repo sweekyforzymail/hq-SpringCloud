d16a6b5214da900469b8474323f3c1e09cff37ec
sweekyforzymail <sweekyforzymail@sina.com>
2019-10-07 19:00:40 +0800

    2019-10-07 19:00

--- hqcloud-club/src/main/java/com/hq/club/ApplicationClub.java
+++ hqcloud-club/src/main/java/com/hq/club/ApplicationClub.java
@@ -2,12 +2,16 @@ package com.hq.club;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
+@EnableFeignClients("com.hq.club")
+@EnableCircuitBreaker
 public class ApplicationClub {
 
 	public static void main(String[] args) {
